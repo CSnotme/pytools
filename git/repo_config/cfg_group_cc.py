30bc6377f929b191eff0a10365f08bb5217e09df
caoyongfei <w_caoyongfei@xiwang.com>
2025-06-16 10:31:54 +0800

    1

--- git/repo_config/cfg_group_cc.py
+++ git/repo_config/cfg_group_cc.py
@@ -5,9 +5,5 @@ __DEFAULT_LOCAL_DIR = "/Users/caoyongfei/workspace/go/src/projects/xwx/test"
 
 # cc配置中心，一共5个仓库
 GROUP_CONFIG_CC = [
-    CodeUpRepoConfig("/xue/bigclass_jichujiagou_common/config-sdk-go",  __DEFAULT_LOCAL_DIR, 'xwx_syh'),
-    CodeUpRepoConfig("/xue/bigclass_jichujiagou_common/config-sdk",     __DEFAULT_LOCAL_DIR, 'xwx_syh'),
-    CodeUpRepoConfig("/xue/bigclass_jichujiagou_common/configure-web",  __DEFAULT_LOCAL_DIR, 'xwx_syh'),
-    CodeUpRepoConfig("/xue/bigclass_jichujiagou_common/configureadmin", __DEFAULT_LOCAL_DIR, 'xwx_syh'),
-    CodeUpRepoConfig("/xue/bigclass_jichujiagou_common/config-server",  __DEFAULT_LOCAL_DIR, 'xwx_syh'),
+    CodeUpRepoConfig("xxx",  __DEFAULT_LOCAL_DIR, 'master'),
 ]
---
+++ openai/main.py
@@ -0,0 +1,34 @@
+
+
+from openai import OpenAI
+
+
+def main():
+    req_deepseek()
+
+
+def req_deepseek():
+    api_key = "sk-eef29b7f1b07462b8bf9d3d9c3d3a184"
+    client = OpenAI(api_key=api_key, base_url="https://api.deepseek.com")
+
+    response = client.chat.completions.create(
+        timeout=30,
+        model="deepseek-chat",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant"},
+            {"role": "user", "content": "想要拥有”睡后收入“的核心是什么"},
+        ],
+        stream=False
+    )
+
+    # for chunk in response.response.iter_bytes():
+        # print(chunk)
+    # print(response.response.json())
+    print(response.choices[0].message.content)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
